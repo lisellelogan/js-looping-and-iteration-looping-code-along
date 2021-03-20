032abbf24bce800f96c2793d12e7324fe2a55ac9
lisellelogan <liselle.logan@yahoo.com>
2021-03-19 21:37:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(startNumber) {
+    while (startNumber > 0) {
+        console.log(startNumber);
+        startNumber -=1
+    }
+    console.log(startNumber);
+}
